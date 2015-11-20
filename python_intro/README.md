934f9d514bf54e47b628776a1ddb5e0787b0a364
Ardi Mehist <ardi@mehist.com>
2015-11-20 14:07:55 +0000

    coursework

--- python_intro/README.md
+++ python_intro/README.md
@@ -2,5 +2,9 @@
 
 Intro to Python:
 
-1. break_time.py (open browser every n seconds)
-2. rename_files.py (remove numbers from file name in prank.zip)
+1. Lesson 1
+  1. break_time.py (open browser every n seconds)
+  2. rename_files.py (remove numbers from file name in prank.zip)
+
+2. Lesson 2
+  1. drawings.py (introduction to classes by drawing shapes)
--- python_intro/rename_files.py
+++ python_intro/rename_files.py
@@ -1,4 +1,14 @@
 import os
 
 def rename_files():
+    # Get file names in directory
+    file_list = os.listdir("/Users/ardimehist/github/udacity/python_intro/prank")
+    print(file_list)
+    saved_path = os.getcwd()
+    os.chdir("/Users/ardimehist/github/udacity/python_intro/prank")
+    # Rename all files
+    for file_name in file_list:
+        os.rename(file_name, file_name.translate(None, "0123456789"))
+    os.chdir(saved_path)
     
+rename_files()
