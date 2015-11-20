e5205369d97c54eb8523a4ec23b5506c98226ffb
Ardi Mehist <ardi@mehist.com>
2015-11-20 19:39:25 +0000

    Coursework
    Change from one function to class and function

--- python_intro/drawings.py
+++ python_intro/drawings.py
@@ -1,29 +1,25 @@
 import turtle
 
-def draw_square():
+def draw_square(some_turtle):
+    for i in range(1, 5):
+        some_turtle.forward(100)
+        some_turtle.right(90)
+
+def draw_art():
     window = turtle.Screen()
     window.bgcolor("red")
-
+    # Create turtle Brad - draw a square
     brad = turtle.Turtle()
     brad.shape("circle")
     brad.color("yellow")
     brad.speed(2)
-
-    brad.forward(100)
-    brad.right(90)
-    brad.forward(100)
-    brad.right(90)
-    brad.forward(100)
-    brad.right(90)
-    brad.forward(100)
-    brad.right(90)
-
+    draw_square(brad)
+    # Create turtle Angie - draw a circle
     angie = turtle.Turtle()
     angie.shape("arrow")
     angie.color("blue")
-
     angie.circle(100)
 
     window.exitonclick()
 
-draw_square()
+draw_art()
