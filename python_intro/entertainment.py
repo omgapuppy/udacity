b6846148d6f2b0248c39fe140e3d582c0155a78b
Ardi Mehist <ardi@mehist.com>
2015-11-22 21:11:06 +0000

    test created class

--- python_intro/entertainment.py
+++ python_intro/entertainment.py
@@ -1,3 +1,8 @@
 import media
 
-guardians_galaxy = media.Movie()
+guardians_galaxy = media.Movie("Guardians of the Galaxy",
+                                "Awesome space raccoon shooting shit",
+                                "http://cdn.collider.com/wp-content/uploads/guardians-of-the-galaxy-poster-star-lord.jpg",
+                                "https://www.youtube.com/watch?v=6lVH3ogPE0w")
+
+print(guardians_galaxy.storyline)
