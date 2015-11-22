de69e16f6ffea2051dcec313660183f87fd88403
Ardi Mehist <ardi@mehist.com>
2015-11-22 23:41:41 +0000

    method overriding

--- python_intro/inheritance.py
+++ python_intro/inheritance.py
@@ -13,10 +13,14 @@ class Child(Parent):
         print("Child constructor called")
         Parent.__init__(self, last_name, eye_color)
         self.number_of_toys = number_of_toys
+# Child subclass show_info overrides the inherited method when called by child
+    def show_info(self):
+        print("Surname: " + self.last_name)
+        print("Eyes: " + self.eye_color)
+        print("Toys: " + str(self.number_of_toys))
 
 # Creating instances in class file ONLY for demo
 billy_cyrus = Parent("Cyrus", "blue")
 billy_cyrus.show_info()
 miley_cyrus = Child("Cyrus", "blue", 5)
-print(miley_cyrus.last_name)
-print(miley_cyrus.number_of_toys)
+miley_cyrus.show_info()
