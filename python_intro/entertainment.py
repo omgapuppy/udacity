d96fd2b6b71be887ff26dc1b13bfe7a262d9a06a
Ardi Mehist <ardi@mehist.com>
2015-11-22 21:21:20 +0000

    create more instances of custom class

--- python_intro/entertainment.py
+++ python_intro/entertainment.py
@@ -6,3 +6,9 @@ guardians_galaxy = media.Movie("Guardians of the Galaxy",
                                 "https://www.youtube.com/watch?v=6lVH3ogPE0w")
 
 print(guardians_galaxy.storyline)
+
+avatar = media.Movie("Avatar",
+                    "Naked blue aliens, being all hippy",
+                    "http://www.goldposter.com/wp-content/uploads/2015/05/Avatar_poster_goldposter_com_56.jpg",
+                    "https://www.youtube.com/watch?v=cRdxXPV9GNQ")
+print(avatar.storyline)
